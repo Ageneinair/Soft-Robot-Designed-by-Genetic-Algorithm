fabcb82ce68e4e8ed173342395e22acd7412436c
wcz1996 <charles.wangcz@hotmail.com>
2018-11-19 14:58:38 -0500

    Initial Commit

---
+++ HW3/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HW3
+//
+//  Created by CHENZHANG WANG on 11/19/18.
+//  Copyright © 2018 CHENZHANG WANG. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
